9fbd0628f6180cac00a17540c0fd0316d81f1c64
Caroline Hermans <carooolinehermans@gmail.com>
2018-01-04 01:50:45 -0500

    make font sizes larger, remove last bits of orange fixes #4 fixes #5 fixes #3

--- chrome/content.js
+++ chrome/content.js
@@ -1,6 +1,15 @@
 document.getElementsByTagName("img")[0].src = chrome.extension.getURL("hn-logo-cute.png")
 document.getElementsByClassName("pagetop")[0].innerHTML = document.getElementsByClassName("pagetop")[0].innerHTML.split("|").join(" ")
 document.getElementsByClassName("pagetop")[1].innerHTML = document.getElementsByClassName("pagetop")[1].innerHTML.split("|").join(" ")
+for (let i = 0; i < document.getElementsByTagName("TD").length; i++) {
+  if (document.getElementsByTagName("TD")[i].getAttribute("bgcolor") === "#ff6600")
+    document.getElementsByTagName("TD")[i].setAttribute("bgcolor", "#fbbfdf")
+}
+
+for (let i = 0; i < document.getElementsByTagName("font").length; i++) {
+  if (document.getElementsByTagName("font")[i].getAttribute("color") === "#ff6600")
+    document.getElementsByTagName("font")[i].setAttribute("color", "#FF83C6")
+}
 
 let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
 link.type = 'image/x-icon';
